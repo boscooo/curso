9af15c74ee6d54404585dd0560319a1fe6e61e91
rodrigo <rcardosom@gmail.com>
2013-11-11 11:49:28 -0300

    Alterando Hello.java

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello {
    
    public static void main(String [] args){
        System.out.println("Ola mundo");
-       System.out.println("Opa - nova linha!");
+       System.out.println("Opa2 - nova linha!");
    }
 
 }
