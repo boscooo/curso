c79e4a9f52d9cb5cc1c94c58e59bc11b2d726e47
rodrigo <rcardosom@gmail.com>
2013-11-11 14:28:14 -0300

    Alteracao 4

--- Hello.java
+++ Hello.java
@@ -4,6 +4,7 @@ public class Hello {
    public static void main(String [] args){
        System.out.println("Ola mundo");
        System.out.println("Opa 2 - nova linha!");
+       System.out.println("Opa 4 - essa veio do master");
    }
 
 }
